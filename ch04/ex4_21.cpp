6cebdd3c061ddc7e434315ffd5f4c72fe2499f0d
iui <iui@iui.com>
2021-05-27 16:47:01 +0800

    Expressions

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -5,14 +5,28 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-int main()
-{
-    vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int main() {
+  vector<int> ivec{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    for (auto i : ivec) {
-        cout << ((i & 0x1) ? i * 2 : i) << " ";
-    }
-    cout << endl;
-    
-    return 0;
+  for (auto i : ivec) {
+    cout << ((i & 0x1) ? i * 2 : i) << " ";
+  }
+  // even
+  //  cout << (9 & 0x1)<<endl ;
+  short svalue = 32767;
+  ++svalue; // -32768
+  cout << endl << svalue << endl;
+
+  unsigned uivalue = 0;
+  --uivalue; // 4294967295
+  cout << uivalue << endl;
+
+  unsigned short usvalue = 65535;
+  ++usvalue; // 0
+  cout << usvalue << endl;
+
+  const char *cp = "hello";
+  cout << (*cp) << endl; // h
+                         //  if (p = getPtr() != 0);
+  return 0;
 }
--- ch04/ex4_22.cpp
+++ ch04/ex4_22.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
-using std::cout; using std::cin; using std::endl;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
-int main()
-{
-    for (unsigned g; cin >> g; )
-    {
-        // conditional operators
-        auto result = g > 90 ? "high pass" : g < 60 ? "fail" : g < 75 ? "low pass" : "pass";
-        cout << result << endl;
+int main() {
+  for (unsigned g; cin >> g;) {
+    // conditional operators
+    auto result = g > 90   ? "high pass"
+                  : g < 60 ? "fail"
+                  : g < 75 ? "low pass"
+                           : "pass";
+    cout << result << endl;
 
-        // if statements
-        if (g > 90)         cout << "high pass";
-        else if (g < 60)    cout << "fail";
-        else if (g < 75)    cout << "low pass";
-        else                cout << "pass";
-        cout << endl;
-    }
-
-    return 0;
+    // if statements
+    if (g > 90)
+      cout << "high pass";
+    else if (g < 60)
+      cout << "fail";
+    else if (g < 75)
+      cout << "low pass";
+    else
+      cout << "pass";
+    cout << endl;
+  }
+  int i; double d; const string *ps; char *pc; void *pv;
+  pv = (void*)ps; // pv = const_cast<string*>(ps); or pv = static_cast<void*>(const_cast<string*>(ps));
+  pv=const_cast<string*>(ps);
+  i = int(*pc);   // i = static_cast<int>(*pc);
+  i=static_cast<int>(*pc);
+  pv = &d;        // pv = static_cast<void*>(&d);
+  pv = static_cast<void*>(&d);
+  pc = (char*)pv; // pc = static_cast<char*>(pv);
+  pc = static_cast<char*>(pv);
+  return 0;
 }
