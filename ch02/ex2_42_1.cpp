1d2b1081298ab55b4276f6e45a8a7746de5cf587
iui <iui@iui.com>
2021-05-27 15:04:05 +0800

    Variables and Basic Types

--- ch02/ex2_42_1.cpp
+++ ch02/ex2_42_1.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include "ex2_42.h"
+#include <iostream>
 
-int main()
-{
-    Sales_data book;
-    double price;
-    std::cin >> book.bookNo >> book.units_sold >> price;
-    book.CalcRevenue(price);
-    book.Print();
+int main() {
+  Sales_data book;
+  double price;
+  std::cin >> book.bookNo >> book.units_sold >> price;
+  book.CalcRevenue(price);
+  book.Print();
 
-    return 0;
+  return 0;
 }
