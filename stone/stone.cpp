7d7cd99b7fa395cca8630157d6539f868d176dba
iui <iui@iui.com>
2021-05-30 01:50:32 +0800

    add stone demo

--- stone/stone.cpp
+++ stone/stone.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "stone.h"
+#include "iostream"
+
+int main() {
+    stone s1(1, 2, 6);
+    stone s2(2, 5, 8);
+    auto s3 = min(s1, s2);
+    std::cout << s3.getWeigh() << std::endl;
+    std::cout << (s1 < s2) << std::endl;
+}
--- stone/stone.h
+++ stone/stone.h
@@ -5,12 +5,23 @@
 #ifndef CPP_PRIMER_STONE_H
 #define CPP_PRIMER_STONE_H
 
-
+template<class T>
+inline const T &min(const T &a, const T &b) {
+    return b < a ? b : a;
+}
 
 class stone {
+public:
+    stone(int w, int h, int we) : _w(w), _h(h), _weigh(we) {};
 
+    bool operator<(const stone &rhs) const {
+        return _weigh < rhs._weigh;
+    }
+
+    int getWeigh() { return _weigh; };
+private:
+    int _w, _h, _weigh;
 };
-
 
 
 #endif //CPP_PRIMER_STONE_H
