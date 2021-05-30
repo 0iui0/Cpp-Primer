6d04cdab0c22d4d2ad73d8fb31e7b472f6851e0f
iui <iui@iui.com>
2021-05-30 11:11:58 +0800

    Specialized Tools and Techniques

--- ch19/ex19_18.cpp
+++ ch19/ex19_18.cpp
@@ -4,20 +4,19 @@
 #include <functional>
 #include <vector>
 
-int main(int argc, char *argv[])
-{
-	std::vector< std::string > v;
-	std::string a;
-	while (std::getline(std::cin,a)) {
-		v.push_back(a);	
-	}
-	auto m = std::count_if(v.begin(),v.end(),std::mem_fn(&std::string::empty));
-	std::cout << "Using mem_fn,the number of empty lines:" << m << std::endl;
-	auto n = std::count_if(v.begin(),v.end(),std::bind(&std::string::empty,std::placeholders::_1));
-	std::cout << "Using bind,the number of empty lines:" << n << std::endl;
-	auto q = std::count_if(v.begin(),v.end(),[](std::string &tem){
-			return tem.empty();
-			});
-	std::cout << "Using lamba,the number of empty lines:" << q << std::endl;
-	return 0;
+int main(int argc, char *argv[]) {
+    std::vector<std::string> v;
+    std::string a;
+    while (std::getline(std::cin, a)) {
+        v.push_back(a);
+    }
+    auto m = std::count_if(v.begin(), v.end(), std::mem_fn(&std::string::empty));
+    std::cout << "Using mem_fn,the number of empty lines:" << m << std::endl;
+    auto n = std::count_if(v.begin(), v.end(), std::bind(&std::string::empty, std::placeholders::_1));
+    std::cout << "Using bind,the number of empty lines:" << n << std::endl;
+    auto q = std::count_if(v.begin(), v.end(), [](std::string &tem) {
+        return tem.empty();
+    });
+    std::cout << "Using lamba,the number of empty lines:" << q << std::endl;
+    return 0;
 }
--- ch19/ex19_3_4.cpp
+++ ch19/ex19_3_4.cpp
@@ -50,53 +50,53 @@ using namespace std;
 
 class A {
 public:
-	virtual ~A() {}
+    virtual ~A() {}
 };
 
 class B : public A {
 public:
-	virtual ~B() {}
+    virtual ~B() {}
 };
 
 class C : public B {
 public:
-	virtual ~C() {}
+    virtual ~C() {}
 };
 
 class D : public B, public A {
 public:
-	virtual ~D() {}
+    virtual ~D() {}
 };
 
-int main(int argc, char const *argv[])
-{
-	/* Exercise 19.3 */
+int main(int argc, char const *argv[]) {
+    /* Exercise 19.3 */
 
-	A *pa = new C;    
-	B *pb = dynamic_cast< B* >(pa); 
-	if (pb) cout << "19.3 (a) succeed!" << endl;
-	else cout << "19.3 (a) fail!" << endl;
+    A *pa = new C;
+    B *pb = dynamic_cast< B * >(pa);
+    if (pb) cout << "19.3 (a) succeed!" << endl;
+    else cout << "19.3 (a) fail!" << endl;
 
-	pb = new B;    
-	C *pc = dynamic_cast< C* >(pb); 
-	if (pc) cout << "19.3 (b) succeed!" << endl;
-	else cout << "19.3 (b) fail!" << endl;
+    pb = new B;
+    C *pc = dynamic_cast< C * >(pb);
+    if (pc) cout << "19.3 (b) succeed!" << endl;
+    else cout << "19.3 (b) fail!" << endl;
 
 /*	pa = new D;    
 	pb = dynamic_cast< B* >(pa); */
 
-	/* Exercise 19.4 */
+    /* Exercise 19.4 */
 
-	C c; B b;
-	A &ra1 = c, &ra2 = b;
-	try {
-		/* succeed */
-		C &rc1 = dynamic_cast<C&>(ra1);
-		cout << "19.4 succeed!" << endl;
-		/* fail */
-		C &rc2 = dynamic_cast<C&>(ra2);
-	} catch (bad_cast) {
-		cout << "19.4 failed!" << endl;
-	}
-	return 0;
+    C c;
+    B b;
+    A &ra1 = c, &ra2 = b;
+    try {
+        /* succeed */
+        C &rc1 = dynamic_cast<C &>(ra1);
+        cout << "19.4 succeed!" << endl;
+        /* fail */
+        C &rc2 = dynamic_cast<C &>(ra2);
+    } catch (bad_cast) {
+        cout << "19.4 failed!" << endl;
+    }
+    return 0;
 }
